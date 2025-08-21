cba57e92b868fa8ff041dd24703c7e45e437fbdc
Guillem Arias <hello@gariasf.com>
2025-08-21 21:48:24 +0200

    feat: styling and basic layout

--- base-fakts/App.js
+++ base-fakts/App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, StyleSheet, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+     <View style={styles.inputContainer}>
+       <TextInput placeholder='Za goal' style={styles.textInput}  />
+       <Button title="Add ze goal" />
+     </View>
+     <View style={styles.goalsContainer}>
+       <Text>List of goals...</Text>
+     </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginBottom: 24
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: "70%",
+    marginRight: 8,
+    paddingLeft: 8
+  },
+  goalsContainer: {
+    flex: 8
+  }
 });
