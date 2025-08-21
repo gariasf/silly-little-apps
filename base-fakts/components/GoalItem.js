b46168b8cf356a3c9592653ece9d5d2c22cf9654
Guillem Arias <hello@gariasf.com>
2025-08-21 22:27:43 +0200

    feat: simple pressed feedback

--- base-fakts/components/GoalItem.js
+++ base-fakts/components/GoalItem.js
@@ -2,22 +2,29 @@ import { Text, StyleSheet, View, Pressable } from 'react-native';
 
 export default function GoalItem({ item, onDelete }) {
   return (
-    <Pressable onPress={onDelete.bind(null, item.key)}>
       <View style={styles.goalItem}>
-        <Text style={styles.goalText}>{item.text}</Text>
+        <Pressable
+          android_ripple={{ color: '#210644' }}
+          onPress={onDelete.bind(null, item.key)}
+          style={({pressed}) => pressed && styles.pressedItem}
+        >
+          <Text style={styles.goalText}>{item.text}</Text>
+        </Pressable>
       </View>
-    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
+    padding: 8,
     color: '#fff'
   }
 });
